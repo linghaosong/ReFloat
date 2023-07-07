51712025bd6c0e4efab832aedea7acf6ed0550f1
linghaosong <linghaosong@cs.ucla.edu>
2023-07-07 02:35:09 +0000

    a sample matrix and a script to download matrices

---
+++ matrices/delete.sh
@@ -0,0 +1,12 @@
+rm -rf crystm01*
+rm -rf minsurfo*
+rm -rf crystm02*
+rm -rf shallow_water1*
+rm -rf wathen100*
+rm -rf gridgena*
+rm -rf wathen120*
+#rm -rf crystm03*
+rm -rf thermomech_TC*
+rm -rf Dubcova2*
+rm -rf thermomech_dM*
+rm -rf qa8fm*
---
+++ matrices/download.sh
@@ -0,0 +1,27 @@
+# download the matrices from SuiteSaprse
+wget https://suitesparse-collection-website.herokuapp.com/MM/Boeing/crystm01.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/GHS_psdef/minsurfo.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/Boeing/crystm02.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/MaxPlanck/shallow_water1.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/GHS_psdef/wathen100.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/GHS_psdef/gridgena.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/GHS_psdef/wathen120.tar.gz
+#wget https://suitesparse-collection-website.herokuapp.com/MM/Boeing/crystm03.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/Botonakis/thermomech_TC.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/UTEP/Dubcova2.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/Botonakis/thermomech_dM.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/Cunningham/qa8fm.tar.gz
+
+# unpackage
+tar -xvf crystm01.tar.gz
+tar -xvf minsurfo.tar.gz
+tar -xvf crystm02.tar.gz
+tar -xvf shallow_water1.tar.gz
+tar -xvf wathen100.tar.gz
+tar -xvf gridgena.tar.gz
+tar -xvf wathen120.tar.gz
+#tar -xvf crystm03.tar.gz
+tar -xvf thermomech_TC.tar.gz
+tar -xvf Dubcova2.tar.gz
+tar -xvf thermomech_dM.tar.gz
+tar -xvf qa8fm.tar.gz
